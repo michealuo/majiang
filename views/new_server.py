19e7af0622ebf98e80b2feb0db8f5c9571f50ce1
michealuo <409081817@qq.com>
2019-11-28 09:57:13 +0800

    将套接字属性封装进类

--- views/new_server.py
+++ views/new_server.py
@@ -8,16 +8,23 @@ ADDR = (HOST, PORT)
 
 
 class Server(Thread):
-    def __init__(self, connfd, list_user):
-        self.connfd = connfd
-        self.list_user = list_user
-
+    def __init__(self):
+        self.c = None
+        self.list_user = []
         super().__init__()
+        self.create_socket()
+    def create_socket(self):
+        # 创建报节字
+        self.s = socket()
+        self.s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.s.bind(ADDR)
+        self.s.listen(3)
 
     def run(self):  # 处理请求
-        self.do_request(self.connfd, self.list_user)
+        self.handle()
 
-
+    def handle(self):
+        pass
 
     def do_request(self,c,l):  # 收消息
         while True:
@@ -38,46 +45,36 @@ class Server(Thread):
             #print(data.decode())
 
 
-def main():
-    # 创建报节字
-    s = socket()
-    s.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
-    s.bind(ADDR)
-    s.listen(3)
-    list_user=[]
+if __name__ == '__main__':
+    #初始化服务器
+    server = Server()
     # 循环创建线程接入客户
     while True:
 
         try:
-            c, addr = s.accept()
-            client_info = (c, addr)
-            list_user.append(client_info)
-            print(list_user)
+            server.c, addr = server.s.accept()
+            client_info = (server.c, addr)
+            server.list_user.append(client_info)
+            print(server.list_user)
             print('connect from:', addr)
 
         except KeyboardInterrupt:
-
-
             sys.exit('退出游戏')
         except OSError:
             sys.exit('退出游戏')
         except Exception as e:
             print(e)
             continue
-        t = Server(c, list_user)
-        t.setDaemon(True)
-        t.start()
+
+        server.setDaemon(True)
+        server.start()
         sleep(0.1)
-        if len(list_user) >= 2:
+        if len(server.list_user) >= 2:
 
 
-            list_user[0][0].send(b'Pok')
-            del list_user[0]
-            list_user[0][0].send(b'Pok')
-            del list_user[0]
+            server.list_user[0][0].send(b'Pok')
+            del server.list_user[0]
+            server.list_user[0][0].send(b'Pok')
+            del server.list_user[0]
 
-            print(list_user)
-
-
-if __name__ == '__main__':
-    main()
+            print(server.list_user)
